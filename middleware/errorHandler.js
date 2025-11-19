5caf0cd58ea3920260992e649fcbbebe20e4ae92
Freddy Pena <fpena@brightoasisfarm.com>
2025-11-19 18:26:56 -0500

    cleaned up and removal

--- middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-module.exports = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.statusCode || 500).json({
-    success: false,
-    message: err.message || 'Server Error'
-  });
-};
--- routes/customers.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
-const { getAllCustomers, getCustomer, createCustomer, updateCustomer, deleteCustomer } = require('../controllers/resourceControllers');
-
-router.route('/').get(protect, getAllCustomers).post(protect, createCustomer);
-router.route('/:id').get(protect, getCustomer).put(protect, updateCustomer).delete(protect, deleteCustomer);
-
-module.exports = router;
